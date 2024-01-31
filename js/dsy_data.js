3178bab271992aeaa48f7be0d4e0dfee4551e5af
shareBeautiful <1454167941@qq.com>
2024-01-31 22:44:57 +0800

    gw

--- js/dsy_data.js
+++ js/dsy_data.js
@@ -35,37 +35,37 @@ let dsy_data = {
     // 产品与服务
     productShow: [
         {
-            img: 'product_1.jpg',
-            desc: 'XPremium quality',
+            img: 'product_3.jpg',
+            desc: '★ Premium quality',
             info: '10cm aluminum umbrella pole.\n' +
                 'Umbrella pole wall thickness 3.2mm\n' +
                 'Umbrella rib material. Aluminum alloy frame.Guaranteed not to fade within 6 years'
         },
         {
-            img: 'product_2.jpg',
-            desc: 'Built-in LED light',
+            img: 'product_4.jpg',
+            desc: '★ Built-in LED light',
             info: '10cm aluminum umbrella poleUmbrella wall thickness 2.48mmUmbrella rib material. carbon steel frame with LED lightGuaranteed not to fade within 5 years'
         },
         {
-            img: 'product_3.jpg',
-            desc: 'Uehides Tents',
+            img: 'product_5.jpg',
+            desc: '★ Uehides Tents',
             info: '10cm aluminum umbrella pole\n' +
                 'Umbrella pole wall thickness 2.48mm Umbrella rib material thickened carbon\n' +
                 'steel frame\n' +
                 'Guaranteed not to fade within 5 years'
         },
         {
-            img: 'product_4.jpg',
-            desc: 'Roucing Chairs',
+            img: 'product_6.jpg',
+            desc: '★ Roucing Chairs',
             info: '7.5cm aluminum umbrella pole.Umbrella pole wal thickness 2.2mm Umbrella rib material: carbon steel frame\n' +
                 'Guaranteed not to fade within 2 years'
         },
         {
-            img: 'product_5.jpg',
+            img: 'product_1.jpg',
             desc: 'Cast Aluminum Tabs And Chais'
         },
         {
-            img: 'product_6.jpg',
+            img: 'product_2.jpg',
             desc: 'Beach Lounge'
         },
         {
@@ -96,8 +96,7 @@ let dsy_data = {
 
     // 关于我们
     aboutUs: [
-        'DSY TECH CO.LTD, is a leading company in the parasol industrybased in Shenzhen, GD. With our commitment to innovation andquality, we strive to provide the best parasol solutions for ourcustomers.',
-        'At DSY TECH CO.LTD., we specialize in designing and manufacturinghigh-quality parasols that combine functionality, durability, andstyle. Our team of experts is dedicated to creating innovativedesigns that not only provide protection from the sun but alsoenhance outdoor spaces. With our extensive range of parasols, wecater to various needs and preferences, ensuring customersatisfaction every time.'
+        'DSY TECH CO.LTD, is a leading company in the parasol industrybased in Shenzhen, GD. With our commitment to innovation andquality, we strive to provide the best parasol solutions for ourcustomers.At DSY TECH CO.LTD., we specialize in designing and manufacturinghigh-quality parasols that combine functionality, durability, andstyle. Our team of experts is dedicated to creating innovativedesigns that not only provide protection from the sun but alsoenhance outdoor spaces. With our extensive range of parasols, wecater to various needs and preferences, ensuring customersatisfaction every time.'
     ],
 
     // 企业文化
